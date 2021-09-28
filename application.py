9fe6c70f64d62be929923e51f0a6616c0ba6c24a
AmirSalari <salari.m.amir@gmail.com>
2021-09-27 20:52:36 -0500

    CSS problem has been solved - static directory is added

--- application.py
+++ application.py
@@ -6,16 +6,16 @@ app = Flask(__name__, template_folder="templates")
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 
-@app.route("/", methods = ["GET", "POST"])
+@app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
         if not request.form.get("text"):
-                error = "Please provide a text to check"
-                return render_template("index.html", text = error)
-        
+            error = "Please provide a text to check"
+            return render_template("index.html", text=error)
+
         text = request.form.get("text")
-        return render_template("/index.html", text = text)
-    
+        return render_template("/index.html", text=text)
+
     else:
         return render_template("/index.html")
 
