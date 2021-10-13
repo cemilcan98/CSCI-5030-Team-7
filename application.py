2bc718deae8ce68fb5237d84d25ce755f131dc9b
AmirSalari <salari.m.amir@gmail.com>
2021-10-12 21:39:23 -0500

    new feature example

--- application.py
+++ application.py
@@ -5,6 +5,7 @@ from hunspell import Hunspell
 import string
 import sys
 import langDetect
+import random
 
 
 app = Flask(__name__, template_folder="templates")
@@ -52,6 +53,20 @@ def index():
         elif request.form["submit_button"] == "clear":
             return render_template("/index.html")
 
+        elif request.form["submit_button"] == "example":
+
+            example_dict = {
+                1: 'What can I do with this?',
+                2: "As you can easily notice the second block of text looks more realistic.",
+                3: "Sam is looking for a job.",
+                4: "I need to buy some things from IKEA.",
+                5: "I hope to visit Peru again in the future."
+            }
+
+            #rand_num = random.randint(0, len(example_arr))
+            example_text = example_dict[1]
+            print(example_text)
+            return render_template("/index.html", example_text=example_text)
     else:
         return render_template("/index.html")
 
