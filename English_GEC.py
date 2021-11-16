a96a77022d2e2bd064c0240863bce03279d3287d
AmirSalari <salari.m.amir@gmail.com>
2021-11-16 15:54:27 -0600

    saving gec=python

--- English_GEC.py
+++ English_GEC.py
@@ -2,4 +2,6 @@ from GEC_python import predict
 
 text = "I wonder if I should take a risk"
 out = predict(text)
+out2 = str(out)
 print("output is: ", out)
+print("output without brackets is: ", out2[2:-8])
