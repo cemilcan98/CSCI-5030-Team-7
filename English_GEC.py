d39918160fbd3c749220381b0d58ca1848d5efc7
AmirSalari <salari.m.amir@gmail.com>
2021-11-17 09:24:52 -0600

    save3-1

--- English_GEC.py
+++
@@ -1,7 +0,0 @@
-from GEC_python import predict
-
-text = "I wonder if I should take a risk"
-out = predict(text)
-out2 = str(out)
-print("output is: ", out)
-print("output without brackets is: ", out2[2:-8])
--- README.md
+++
@@ -1,5 +0,0 @@
-# CSCI-5030-Team-7
-
-CSCI 5030 - Context Sensitive Spelling Detection 
-
-testing 1 2 3 4 5 git 
